e745ee24b52fa990798d2c84372aa1b57c2cfae6
Jaydesire <elgovsky@ya.ru>
2022-07-25 12:52:17 +0300

    Upd the algorithm of the program

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,61 @@
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.LongAdder;
 
 public class Main {
-    public static AtomicReference<BigDecimal> totalRevenue = new AtomicReference<>();
-    public static void main(String[] args) {
-        totalRevenue.set(new BigDecimal(0));
-        int qtyShops = 3;
-        Shop[] shops = new Shop[qtyShops];
 
-        for (int i = 0; i < shops.length; i++) {
-            Shop shop = new Shop();
-            shops[i] = shop;
-            shop.start();
+    static LongAdder totalRevenue = new LongAdder();
+
+    public static void main(String[] args)  {
+
+        int[][] shopsRevenue = {
+                {1_000, 2_000, 3_000},
+                {4_000, 10_000, 6_000},
+                {7_000, 8_000, 9_000}
+        };
+
+        List<Thread> threads = new ArrayList<>();
+
+        //Создание потоков
+        for (int i = 0; i < shopsRevenue.length; i++) {
+            int finalI = i;
+            threads.add(
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Генерация массива выручки по каждому магазину
+                            int[] shopRevenue = new int[shopsRevenue[finalI].length];
+                            for (int j = 0; j < shopsRevenue[finalI].length; j++) {
+                                shopRevenue[j] = shopsRevenue[finalI][j];
+                            }
+                            calculateRevenue(shopRevenue);
+                        }
+                    })
+            );
         }
 
-        for (int i = 0; i < shops.length; i++) {
+        //Старт потоков
+        for (Thread t : threads) {
+            t.start();
+        }
+
+        //Ожидание завершения работы потоков
+        for (Thread t : threads) {
             try {
-                shops[i].join();
-            } catch (InterruptedException e) {}
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+
         System.out.println("Общая выручка составила " + totalRevenue + "₽");
+    }
+
+    public static void calculateRevenue(int[] revenue) {
+        for (int i = 0; i < revenue.length; i++) {
+            totalRevenue.add(revenue[i]);
+        }
     }
 }
--- src/Shop.java
+++ src/Shop.java
@@ -1,19 +1,32 @@
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-public class Shop extends Thread {
-    public ThreadLocal<BigDecimal> calculateRevenue() {
-        var revenue = new ThreadLocal<BigDecimal>();
-        revenue.set(new BigDecimal(Math.random() * 10_000_000).setScale(2, RoundingMode.DOWN));//.setScale(2));
-        System.out.println("Выручка " + Thread.currentThread().getName()
-                + " составила " + revenue.get() + "₽");
+//public class Shop extends Thread {
+//    public ThreadLocal<BigDecimal> calculateRevenue() {
+//        var revenue = new ThreadLocal<BigDecimal>();
+//        revenue.set(new BigDecimal(1).setScale(2, RoundingMode.DOWN));//.setScale(2));
+//        System.out.println("Выручка " + Thread.currentThread().getName()
+//                + " составила " + revenue.get() + "₽");
+//        return revenue;
+//    }
+//
+//    @Override
+//    public void run() {
+//        Main.totalRevenue.set(calculateRevenue()
+//                .get()
+//                .add(Main.totalRevenue.get()));
+//    }
+//}
+
+public class Shop  {
+
+   private int[] revenue;
+    Shop(int[] revenue){
+     this.revenue = revenue;
+    }
+    public int[] getRevenue() {
         return revenue;
     }
 
-    @Override
-    public void run() {
-        Main.totalRevenue.set(calculateRevenue()
-                .get()
-                .add(Main.totalRevenue.get()));
-    }
+
 }
